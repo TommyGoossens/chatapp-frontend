b702a4d73990d4e11c171110432ea2139d2bfabe
Tommy Goossens <tommygoossens@ziggo.nl>
2019-05-22 22:54:08 +0200

    Auto stash before revert of "Added check for navbar, displayed only when logged in"

--- frontend/src/app/services/lobby/lobby.service.ts
+++ frontend/src/app/services/lobby/lobby.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {RestService} from '../rest.service';
+import {AlertService} from '../alert/alert.service';
+import {Observable} from 'rxjs';
+import {Chat} from '../../components/chat-wrapper/chat/models/Chat';
+import {ChatMessage} from '../../components/chat-wrapper/chat/models/ChatMessage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LobbyService {
+  constructor(private restService: RestService) {
 
-  constructor() { }
+  }
+
+
+  /**
+   *
+   */
+  getOpenChats(): Observable<any> {
+    return this.restService.get<ChatMessage[]>('lobby/chats');
+  }
 }
